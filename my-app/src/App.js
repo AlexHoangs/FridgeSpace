1788715eb0e05791b2a2087c9195edc4dc7e7e64
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2023-02-18 09:52:07 -0800

    found code for text processing from an image

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
 import reciept from './reciept.jpeg';
+import img from './img2.png';
+
+import React from 'react';
 
 import Tesseract from 'tesseract.js';
 
 Tesseract.recognize(
-  {reciept},'eng',
-  { 
-    logger: m => console.log(m) 
-  }
-)
-.catch (err => {
-  console.error(err);
-})
-.then(result => {
-  // Get Confidence score
-  let confidence = result.confidence
- 
-  let text = result.text
-  console.log("the text is",text)
-  console.log(confidence)
-
+  'https://tesseract.projectnaptha./img/eng_bw.png',
+  'eng',
+  { logger: m => console.log(m) }
+).then(({ data: { text } }) => {
+  console.log(text);
 })
 
-function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [image, setImage] = React.useState('');
+  const [text, setText] = React.useState('');
+  const [progress, setProgress] = React.useState(0);
+
+  const handleSubmit = () => {
+    setIsLoading(true);
+    Tesseract.recognize(image, 'eng', {
+      logger: (m) => {
+        console.log(m);
+        if (m.status === 'recognizing text') {
+          setProgress(parseInt(m.progress * 100));
+        }
+      },
+    })
+      .catch((err) => {
+        console.error(err);
+      })
+      .then((result) => {
+        console.log(result.data);
+        setText(result.data.text);
+        setIsLoading(false);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={reciept} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100vh' }}>
+      <div>
+        <div>
+          {!isLoading && (
+            <h1 className="text-center py-5 mc-5">Image To Text</h1>
+          )}
+          {isLoading && (
+            <>
+              <progress className="form-control" value={progress} max="100">
+                {progress}%{' '}
+              </progress>{' '}
+              <p className="text-center py-0 my-0">Converting:- {progress} %</p>
+            </>
+          )}
+          {!isLoading && !text && (
+            <>
+              <input
+                type="file"
+                onChange={(e) =>
+                  setImage(URL.createObjectURL(e.target.files[0]))
+                }
+                className="form-control mt-5 mb-2"
+              />
+              <input
+                type="button"
+                onClick={handleSubmit}
+                className="btn btn-primary mt-5"
+                value="Convert"
+              />
+            </>
+          )}
+          {!isLoading && text && (
+            <>
+              <textarea
+                className="form-control w-100 mt-5"
+                rows="30"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+              ></textarea>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
