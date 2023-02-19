ac4df2d29e1c51e3343617ee9e470cb8b878f9df
Sauvikesh <81947858+Sauvikesh@users.noreply.github.com>
2023-02-18 20:01:46 -0800

    reduced spacing

--- my-app/src/components/FoodItem.js
+++ my-app/src/components/FoodItem.js
@@ -2,14 +2,10 @@ function FoodItem(props){
     return(
     <div>
         <div className = "foodItem">
-
             <div className = "itemDesc">
             <img className = "foodPicture" src={props.picture} alt="picture" />
-
             <h1>{props.name}</h1>
-
             <p>Purchase Date: {props.date}</p>
-
             <div>
                 <p className = "dueDate"> {props.due}</p>
             </div>
